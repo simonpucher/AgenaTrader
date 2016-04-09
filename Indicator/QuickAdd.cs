b99e7fddf5a49f74d4e3d542b2db8465c1fd5420
Simon Pucher <simon.pucher@creativo.at>
2016-04-09 13:27:30 +0200

    QuickAdd added

--- Indicator/QuickAdd.cs
+++ Indicator/QuickAdd.cs
@@ -26,7 +26,9 @@ namespace AgenaTrader.UserCode
 		#region Variables
 
 		    private string _name_of_list = String.Empty;
-
+            private IInstrumentsList _list = null;
+            private Rectangle rect;
+            private Pen pen;
 
 		#endregion
 
@@ -39,46 +41,75 @@ namespace AgenaTrader.UserCode
 
         protected override void OnStartUp()
         {
-            base.OnStartUp();
+            //base.OnStartUp();
+
+
+            // Add event listener
+            if (ChartControl != null)
+                ChartControl.ChartPanelMouseDown += OnChartPanelMouseDown;
 
             if (this.Instrument != null)
             {
-                 if (!String.IsNullOrEmpty(Name_of_list))
+                if (!String.IsNullOrEmpty(Name_of_list))
                 {
 
                     this.Root.Core.InstrumentManager.GetInstrumentLists();
-                    IInstrumentsList liste = this.Root.Core.InstrumentManager.GetInstrumentsListStatic(this.Name_of_list);
-                    if (liste == null)
+                    _list = this.Root.Core.InstrumentManager.GetInstrumentsListStatic(this.Name_of_list);
+                    if (_list == null)
                     {
-                        liste = this.Root.Core.InstrumentManager.GetInstrumentsListDynamic(this.Name_of_list);
+                        _list = this.Root.Core.InstrumentManager.GetInstrumentsListDynamic(this.Name_of_list);
                     }
-                    if (liste != null)
-                    {
-                        //Get instrument
-                        // Instrument instrument = this.Root.Core.InstrumentManager.GetInstrument("SKB.DE");
-
-                        // liste.Add(instrument);
-
-                        //If you want to clear instruments from the list
-                        //liste.Clear();
-
-                        if (!this.Root.Core.InstrumentManager.IsInstrumentExists(this.Instrument.Symbol))
-                        {
-                            this.Root.Core.InstrumentManager.AddInstrument2List(this.Instrument, this.Name_of_list);
-                        }
-
-                    }
-                    else
+                    if (_list == null)
                     {
                         Log("The list " + this.Name_of_list + " does not exist.", InfoLogLevel.Warning);
                     }
-
                 }
                 else
                 {
                     Log("You need to specify a name for the list.", InfoLogLevel.Warning);
                 }
             }
+
+
+
+            //if (this.Instrument != null)
+            //{
+            //     if (!String.IsNullOrEmpty(Name_of_list))
+            //    {
+
+            //        this.Root.Core.InstrumentManager.GetInstrumentLists();
+            //        _list = this.Root.Core.InstrumentManager.GetInstrumentsListStatic(this.Name_of_list);
+            //        if (liste == null)
+            //        {
+            //            liste = this.Root.Core.InstrumentManager.GetInstrumentsListDynamic(this.Name_of_list);
+            //        }
+            //        if (liste != null)
+            //        {
+            //            //Get instrument
+            //            // Instrument instrument = this.Root.Core.InstrumentManager.GetInstrument("SKB.DE");
+
+            //            // liste.Add(instrument);
+
+            //            //If you want to clear instruments from the list
+            //            //liste.Clear();
+
+            //            if (!this.Root.Core.InstrumentManager.IsInstrumentExists(this.Instrument.Symbol))
+            //            {
+            //                this.Root.Core.InstrumentManager.AddInstrument2List(this.Instrument, this.Name_of_list);
+            //            }
+
+            //        }
+            //        else
+            //        {
+            //            Log("The list " + this.Name_of_list + " does not exist.", InfoLogLevel.Warning);
+            //        }
+
+            //    }
+            //    else
+            //    {
+            //        Log("You need to specify a name for the list.", InfoLogLevel.Warning);
+            //    }
+            //}
 
 
         }
@@ -93,12 +124,30 @@ namespace AgenaTrader.UserCode
 		{
 			MyPlot1.Set(Input[0]);
 
+            if (this.IsCurrentBarLast && _list != null)
+            {
+                if (_list.Contains((Instrument)this.Instrument))
+                {
+                    //DrawTextFixed("MyText", "is in list", TextPosition.TopRight);
+                    //DrawText("MyText2", "Das ist ein Beispieltext.", 10, 60, Color.Black);
+                }
+                else
+                {
+                    //DrawTextFixed("MyText", "is not list", TextPosition.TopRight);
+                    //DrawText("MyText2", "Das ist ein Beispieltext.", 10, 60, Color.Black);
+                }
+
+                DrawTextFixed("MyText", "Currentbar: " + CurrentBar + " - " + DateTime.Now.ToString(), TextPosition.TopRight);
+
+            }
 
 		}
 
         protected override void OnTermination()
         {
-        
+            // Remove event listener
+            if (ChartControl != null)
+                ChartControl.ChartPanelMouseDown -= OnChartPanelMouseDown;
         }
 
 
@@ -117,12 +166,114 @@ namespace AgenaTrader.UserCode
         }
 
 
+        #region Events
 
-		#region Properties
 
-            #region Output
 
-            [Browsable(false)]
+        public override void Plot(Graphics g, Rectangle r, double min, double max)
+        {
+            if (Bars == null || ChartControl == null) return;
+
+          
+            pen = Pens.Black;
+           rect = new Rectangle(r.Width - 100, 20 , 80, 60);
+            g.DrawRectangle(pen, rect);
+
+            //// Eigenschaften von ChartControl
+            //string s;
+            //s = "bounds: " + r.X.ToString() + "   " + r.Y.ToString() + "   " + r.Height.ToString() + "   " + r.Width.ToString();
+            //g.DrawString(s, font, brush, 10, 50, sf);
+
+            //s = "min: " + Instrument.Round2TickSize(min).ToString() + "   max: " + Instrument.Round2TickSize(max).ToString();
+            //g.DrawString(s, font, brush, 10, 70, sf);
+
+            //s = "BarSpace: " + ChartControl.BarSpace.ToString() + "   BarWidth: " + ChartControl.BarWidth.ToString();
+            //g.DrawString(s, font, brush, 10, 90, sf);
+
+            //s = "Bars.Count: " + Bars.Count.ToString();
+            //g.DrawString(s, font, brush, 10, 110, sf);
+
+            //s = "BarsPainted: " + ChartControl.BarsPainted.ToString() + "   FirstBarPainted: " + ChartControl.FirstBarPainted.ToString() + "   LastBarPainted: " + ChartControl.LastBarPainted.ToString();
+            //g.DrawString(s, font, brush, 10, 130, sf);
+
+            //s = "BarsVisible: " + ChartControl.BarsVisible.ToString() + "   FirstBarVisible: " + ChartControl.FirstBarVisible.ToString() + "   LastBarVisible: " + ChartControl.LastBarVisible.ToString();
+            //g.DrawString(s, font, brush, 10, 150, sf);
+
+
+
+        }
+
+
+            private void OnChartPanelMouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+            {
+                Print("X = {0}, Y = {1}", e.X, e.Y);
+                Print("X = {0}, Y = {1}", ChartControl.GetDateTimeByX(e.X), ChartControl.GetPriceByY(e.Y));
+
+                //ITextFixed txt = (ITextFixed)DrawObjects["MyText"];
+                //IText bubu;
+                
+
+                //Point cursorPos = new Point(e.X, e.Y);
+                //if (rect.Contains(cursorPos))
+                //{
+                //    DrawText("MyText", "Das ist ein Beispieltext.", 10, 3, Color.Black);
+                //    pen = Pens.Red;
+                //}
+                //else
+                //{
+                //    DrawText("MyText", "Das ist ein Beispieltext.", 10, 3, Color.Black);
+                //    pen = Pens.Black;
+                //}
+
+
+                this.OnBarUpdate();
+                
+              
+
+                //if (e.Button == System.Windows.Forms.MouseButtons.Left)
+                //{
+                //    //Get the last order where IsProposal is true.
+                //    if (this.TradingManager != null && olf != null && this.Instrument != null)
+                //    {
+                //        IEnumerable<OrdersLogRecord> olren = this.TradingManager.GetOrdersLog(olf).Where(x => x.Instrument.Id == this.Instrument.Id).Where(x => x.IsProposal == true).Where(x => x.State == OrderState.PendingSubmit);
+                //        List<OrdersLogRecord> hhh = olren.ToList();
+                //        OrdersLogRecord olr = olren.LastOrDefault();
+                //        if (olr != null)
+                //        {
+                //            //Cast the order
+                //            Order ord = (Order)this.TradingManager.GetOrder(olr.OrderId);
+                //            if (ord != null && ord.State == OrderState.PendingSubmit)
+                //            {
+                //                //Change quantity
+                //                double clickprice = ChartControl.GetPriceByY(e.Y);
+                //                if (clickprice >= ord.Price + (ord.Price / 100 * 1))
+                //                {
+                //                    ord.Quantity = ord.Quantity + 1;
+                //                    this.TradingManager.EditOrder(ord);
+                //                }
+                //                else if (clickprice <= ord.Price - (ord.Price / 100 * 1))
+                //                {
+                //                    if (ord.Quantity > 1)
+                //                    {
+                //                        ord.Quantity = ord.Quantity - 1;
+                //                        this.TradingManager.EditOrder(ord);
+                //                    }
+                //                }
+                //            }
+                //        }
+                //    }
+                //}
+            }
+
+        #endregion
+
+
+
+        #region Properties
+
+        #region Output
+
+        [Browsable(false)]
             [XmlIgnore()]
             public DataSeries MyPlot1
             {
@@ -269,3 +420,4 @@ namespace AgenaTrader.UserCode
 }
 
 #endregion
+
