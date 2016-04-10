ae9662e389e7133bfaf2ba0d4b000338a73ac010
Simon Pucher <simon.pucher@creativo.at>
2016-04-10 17:37:17 +0200

    ORB Condition

---
+++ Condition/Open_Range_Breakout_Condition.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Xml;
+using System.Xml.Serialization;
+using AgenaTrader.API;
+using AgenaTrader.Custom;
+using AgenaTrader.Plugins;
+using AgenaTrader.Helper;
+
+/// <summary>
+/// Version: 1.0
+/// -------------------------------------------------------------------------
+/// Simon Pucher 2016
+/// Christian Kovar 2016
+/// -------------------------------------------------------------------------
+/// ToDo
+/// 
+/// 
+/// 
+/// -------------------------------------------------------------------------
+/// Namespace holds all indicators and is required. Do not change it.
+/// </summary>
+namespace AgenaTrader.UserCode
+{
+	[Description("Geben Sie bitte hier die Beschreibung für die neue Condition ein")]
+	[IsEntryAttribute(true)]
+	[IsStopAttribute(false)]
+	[IsTargetAttribute(false)]
+	[OverrulePreviousStopPrice(false)]
+	public class ORB_Condition : UserScriptedCondition
+	{
+		#region Variables
+
+		private int _myCondition1 = 1;
+
+		#endregion
+
+		protected override void Initialize()
+		{
+			IsEntry = true;
+			IsStop = false;
+			IsTarget = false;
+			Add(new Plot(Color.FromKnownColor(KnownColor.Black), "Occurred"));
+			Add(new Plot(Color.FromArgb(255, 187, 128, 238), "Entry"));
+			Overlay = true;
+			CalculateOnBarClose = true;
+		}
+
+		protected override void OnBarUpdate()
+		{
+			//TODO: Write your owner OnBarUpdate handling
+		}
+
+		#region Properties
+
+		[Browsable(false)]
+		[XmlIgnore()]
+		public DataSeries Occurred
+		{
+			get { return Values[0]; }
+		}
+
+		[Browsable(false)]
+		[XmlIgnore()]
+		public DataSeries Entry
+		{
+			get { return Values[1]; }
+		}
+
+		public override IList<DataSeries> GetEntries()
+		{
+			return new[]{Entry};
+		}
+
+		[Description("")]
+		[Category("Parameters")]
+		public int MyCondition1
+		{
+			get { return _myCondition1; }
+			set { _myCondition1 = Math.Max(1, value); }
+		}
+
+		#endregion
+	}
+}
