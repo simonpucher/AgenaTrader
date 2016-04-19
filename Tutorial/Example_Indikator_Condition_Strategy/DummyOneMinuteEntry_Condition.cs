3545472cec83178b4fddea22b820e60dde5853dc
ckovar82 <ckovar82@gmail.com>
2016-04-19 18:13:20 +0200

    Dummy Example Indikator/Condition/Stragetie
    Dummy Example Indikator/Condition/Stragetie

---
+++ Tutorial/Example_Indikator_Condition_Strategy/DummyOneMinuteEntry_Condition.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Xml;
+using System.Xml.Serialization;
+using AgenaTrader.API;
+using AgenaTrader.Custom;
+using AgenaTrader.Plugins;
+using AgenaTrader.Helper;
+
+namespace AgenaTrader.UserCode
+{
+    [Description("Steigt bei jeder geraden Minute mit der jeweils ausgewählten Strategie in den Markt ein. Dient nur zum Strategie-Test >>> DUMMY!!!")]
+    [IsEntryAttribute(true)]
+    [IsStopAttribute(false)]
+    [IsTargetAttribute(false)]
+    [OverrulePreviousStopPrice(false)]
+    public class DummyOneMinuteEntry_Condition : UserScriptedCondition
+    {
+
+        IOrder oEnterLong;
+        IOrder oExitLong;
+
+        private int _myvalue = 1;
+        private DummyOneMinuteEven_Indicator bubu;
+
+        protected override void Initialize()
+        {
+            IsEntry = true;
+            IsStop = false;
+            IsTarget = false;
+            Add(new Plot(Color.FromKnownColor(KnownColor.Black), "Occurred"));
+            Add(new Plot(Color.FromArgb(255, 153, 170, 61), "Entry"));
+            Overlay = true;
+            CalculateOnBarClose = false;
+            ClearOutputWindow();
+
+
+        }
+
+        protected override void OnBarUpdate()
+        {
+            Print(this.Instrument.ToString());
+
+            if (Bars != null && Bars.Count > 0
+                && TimeFrame.Periodicity == DatafeedHistoryPeriodicity.Minute
+                && TimeFrame.PeriodicityValue == 1)
+            { }
+            else
+            {
+                return;
+            }
+
+            if (DummyOneMinuteEven_Indicator()[0] == 100)
+            {
+                Occurred.Set(-1);
+            }
+            else
+            {
+                Occurred.Set(1);
+            }
+
+            //Entry.Set(iv_Bars.GetOpen(iv_CurrentBar));
+            //Entry.Set(10390);
+            //Entry.Set(GetCurrentBid());
+
+        }
+
+        #region Properties
+
+        [Description("Period for RSI")]
+        [Category("Parameters")]
+        [DisplayName("Period for RSI")]
+        public int Myvalue
+        {
+            get { return _myvalue; }
+            set { _myvalue = value; }
+        }
+
+        [Browsable(false)]
+        [XmlIgnore()]
+        public DataSeries Occurred
+        {
+            get { return Values[0]; }
+        }
+
+        [Browsable(false)]
+        [XmlIgnore()]
+        public DataSeries Entry
+        {
+            get { return Values[1]; }
+        }
+
+        public override IList<DataSeries> GetEntries()
+        {
+            return new[] { Entry };
+        }
+
+        #endregion
+    }
+}
--- Utility/GlobalUtilities_Utility.cs
+++ Utility/GlobalUtilities_Utility.cs
@@ -12,7 +12,7 @@ using AgenaTrader.Plugins;
 using AgenaTrader.Helper;
 
 /// <summary>
-/// Version: 1.0
+/// Version: 1.1
 /// -------------------------------------------------------------------------
 /// Simon Pucher 2016
 /// Christian Kovar 2016
@@ -31,77 +31,78 @@ namespace AgenaTrader.UserCode
 
         #region Colors
 
-        /// <summary>
-        /// Adjust the brightness of a color. 
-        /// e.g. use this function to create a similiar color in a button click event or on mouse hover.
-        /// </summary>
-        /// <param name="originalColour"></param>
-        /// <param name="brightnessFactor"></param>
-        /// <returns></returns>
-        public static Color AdjustBrightness(Color originalColour, double brightnessFactor)
-        {
-            return Color.FromArgb(originalColour.A, (int)(originalColour.R * brightnessFactor), (int)(originalColour.G * brightnessFactor), (int)(originalColour.B * brightnessFactor));
-        }
-
-        /// <summary>
-        /// Adjust the opacity of a color. 
-        /// e.g. use this function to change the alpha channel of the Color.
-        /// </summary>
-        /// <param name="originalColour"></param>
-        /// <param name="opacityFactor"></param>
-        /// <returns></returns>
-        public static Color AdjustOpacity(Color originalColour, double opacityFactor)
-        {
-            return Color.FromArgb((int)(originalColour.A * opacityFactor), originalColour.R, originalColour.G, originalColour.B);
-        }
-
-
-        public static TimeSpan GetOfficialMarketOpeningTime(string Symbol)
-        {
-            //Gets official Stock Market Opening Time
-            //Dirty hack to handle different pre-market times
-            //technically we can not distinguish between pre-market and market data
-            //e.g. use this function to determine opening time for Dax-Index (09.00) or Nasdaq-Index(15.30)
-            if (Symbol.Contains("DE.30") || Symbol.Contains("DE-XTB"))
+            /// <summary>
+            /// Adjust the brightness of a color. 
+            /// e.g. use this function to create a similiar color in a button click event or on mouse hover.
+            /// </summary>
+            /// <param name="originalColour"></param>
+            /// <param name="brightnessFactor"></param>
+            /// <returns></returns>
+            public static Color AdjustBrightness(Color originalColour, double brightnessFactor)
             {
-                return new TimeSpan(9, 00, 00);
+                return Color.FromArgb(originalColour.A, (int)(originalColour.R * brightnessFactor), (int)(originalColour.G * brightnessFactor), (int)(originalColour.B * brightnessFactor));
             }
-            else if (Symbol.Contains("US.30") || Symbol.Contains("US-XTB"))
-            {
-                return new TimeSpan(15, 30, 00);
-            }
-            else
-            {
-                return new TimeSpan(9, 00, 00);
-            }
-        }
 
-        public static TimeSpan GetOfficialMarketClosingTime(string Symbol)
-        {
-            //Gets official Stock Market Closing Time
-            //e.g. use this function to determine closing time for Dax-Index (17.30) or Nasdaq-Index(22.00)
-            if (Symbol.Contains("DE.30") || Symbol.Contains("DE-XTB"))
+            /// <summary>
+            /// Adjust the opacity of a color. 
+            /// e.g. use this function to change the alpha channel of the Color.
+            /// </summary>
+            /// <param name="originalColour"></param>
+            /// <param name="opacityFactor"></param>
+            /// <returns></returns>
+            public static Color AdjustOpacity(Color originalColour, double opacityFactor)
             {
-                return new TimeSpan(17, 30, 00);
-
+                return Color.FromArgb((int)(originalColour.A * opacityFactor), originalColour.R, originalColour.G, originalColour.B);
             }
-            else if (Symbol.Contains("US.30") || Symbol.Contains("US-XTB"))
+
+        #endregion
+
+            public static TimeSpan GetOfficialMarketOpeningTime(string Symbol)
             {
-                return new TimeSpan(22, 00, 00);
+                //Gets official Stock Market Opening Time
+                //Dirty hack to handle different pre-market times
+                //technically we can not distinguish between pre-market and market data
+                //e.g. use this function to determine opening time for Dax-Index (09.00) or Nasdaq-Index(15.30)
+                if (Symbol.Contains("DE.30") || Symbol.Contains("DE-XTB"))
+                {
+                    return new TimeSpan(9, 00, 00);
+                }
+                else if (Symbol.Contains("US.30") || Symbol.Contains("US-XTB"))
+                {
+                    return new TimeSpan(15, 30, 00);
+                }
+                else
+                {
+                    return new TimeSpan(9, 00, 00);
+                }
             }
-            else
+
+            public static TimeSpan GetOfficialMarketClosingTime(string Symbol)
             {
-                return new TimeSpan(17, 30, 00);
+                //Gets official Stock Market Closing Time
+                //e.g. use this function to determine closing time for Dax-Index (17.30) or Nasdaq-Index(22.00)
+                if (Symbol.Contains("DE.30") || Symbol.Contains("DE-XTB"))
+                {
+                    return new TimeSpan(17, 30, 00);
+
+                }
+                else if (Symbol.Contains("US.30") || Symbol.Contains("US-XTB"))
+                {
+                    return new TimeSpan(22, 00, 00);
+                }
+                else
+                {
+                    return new TimeSpan(17, 30, 00);
+                }
             }
-        }
 
 
+            public static IBar GetFirstBarOfCurrentSession(IBars Bars, DateTime Date)
+            {
+                //returns the first Bar of the latest(=current) Session
+                return Bars.Where(x => x.Time.Date == Date).FirstOrDefault();
+            }
 
-        public static IBar GetFirstBarOfCurrentSession(IBars Bars, DateTime Date)
-        {
-            //returns the first Bar of the latest(=current) Session
-            return Bars.Where(x => x.Time.Date == Date).FirstOrDefault();
-        }
 
         public static double GetHighestHigh(IBars Bars, int BarsAgo) {
 //HighestHigh Method is not available in Conditions, therefore this alternative can be used
@@ -121,8 +122,6 @@ namespace AgenaTrader.UserCode
             //LowestLow Method is not available in Conditions, therefore this alternative can be used
             double LowestLow = 9999999999;
             for (int i = 0; i < BarsAgo; i++)
-
-<<<<<<< HEAD
                 if (LowestLow > Bars[i].High)
                 {
                     LowestLow = Bars[i].High;
@@ -130,27 +129,209 @@ namespace AgenaTrader.UserCode
             ;
             return LowestLow;
         }
-    }
 
 
-=======
- 
->>>>>>> refs/remotes/simonpucher/master
-        #endregion
 
-        #region DateTime
-        /// <summary>
-        /// Returns the Start of the current week.
-        /// Code from: http://www.codeproject.com/Articles/9706/C-DateTime-Library
-        /// </summary>
-        /// <returns></returns>
-          public static DateTime GetStartOfCurrentWeek()
+
+
+        
+
+        #region DateTimeHelpers taken from  http://www.codeproject.com/Articles/9706/C-DateTime-Library
+
+        public enum Quarter
+        {
+            First = 1,
+            Second = 2,
+            Third = 3,
+            Fourth = 4
+        }
+
+        public enum Month
+        {
+            January = 1,
+            February = 2,
+            March = 3,
+            April = 4,
+            May = 5,
+            June = 6,
+            July = 7,
+            August = 8,
+            September = 9,
+            October = 10,
+            November = 11,
+            December = 12
+        }
+
+            public static DateTime GetStartOfQuarter(int Year, Quarter Qtr)
             {
-                int DaysToSubtract = (int)DateTime.Now.DayOfWeek;
-                DateTime dt = DateTime.Now.Subtract(System.TimeSpan.FromDays( DaysToSubtract ) );
+                if (Qtr == Quarter.First)    // 1st Quarter = January 1 to March 31
+                    return new DateTime(Year, 1, 1, 0, 0, 0, 0);
+                else if (Qtr == Quarter.Second) // 2nd Quarter = April 1 to June 30
+                    return new DateTime(Year, 4, 1, 0, 0, 0, 0);
+                else if (Qtr == Quarter.Third) // 3rd Quarter = July 1 to September 30
+                    return new DateTime(Year, 7, 1, 0, 0, 0, 0);
+                else // 4th Quarter = October 1 to December 31
+                    return new DateTime(Year, 10, 1, 0, 0, 0, 0);
+            }
+
+            public static DateTime GetEndOfQuarter(int Year, Quarter Qtr)
+            {
+                if (Qtr == Quarter.First)    // 1st Quarter = January 1 to March 31
+                    return new DateTime(Year, 3, DateTime.DaysInMonth(Year, 3), 23, 59, 59, 999);
+                else if (Qtr == Quarter.Second) // 2nd Quarter = April 1 to June 30
+                    return new DateTime(Year, 6, DateTime.DaysInMonth(Year, 6), 23, 59, 59, 999);
+                else if (Qtr == Quarter.Third) // 3rd Quarter = July 1 to September 30
+                    return new DateTime(Year, 9, DateTime.DaysInMonth(Year, 9), 23, 59, 59, 999);
+                else // 4th Quarter = October 1 to December 31
+                    return new DateTime(Year, 12, DateTime.DaysInMonth(Year, 12), 23, 59, 59, 999);
+            }
+
+            public static Quarter GetQuarter(Month Month)
+            {
+                if (Month <= Month.March)
+                    // 1st Quarter = January 1 to March 31
+                    return Quarter.First;
+                else if ((Month >= Month.April) && (Month <= Month.June))
+                    // 2nd Quarter = April 1 to June 30
+                    return Quarter.Second;
+                else if ((Month >= Month.July) && (Month <= Month.September))
+                    // 3rd Quarter = July 1 to September 30
+                    return Quarter.Third;
+                else // 4th Quarter = October 1 to December 31
+                    return Quarter.Fourth;
+            }
+
+            public static DateTime GetEndOfLastQuarter()
+            {
+                if ((Month)DateTime.Now.Month <= Month.March)
+                    //go to last quarter of previous year
+                    return GetEndOfQuarter(DateTime.Now.Year - 1, Quarter.Fourth);
+                else //return last quarter of current year
+                    return GetEndOfQuarter(DateTime.Now.Year, GetQuarter((Month)DateTime.Now.Month));
+            }
+
+            public static DateTime GetStartOfLastQuarter()
+            {
+                if ((Month)DateTime.Now.Month <= Month.March)
+                    //go to last quarter of previous year
+                    return GetStartOfQuarter(DateTime.Now.Year - 1, Quarter.Fourth);
+                else //return last quarter of current year
+                    return GetStartOfQuarter(DateTime.Now.Year, GetQuarter((Month)DateTime.Now.Month));
+            }
+
+            public static DateTime GetStartOfCurrentQuarter()
+            {
+                return GetStartOfQuarter(DateTime.Now.Year, GetQuarter((Month)DateTime.Now.Month));
+            }
+
+            public static DateTime GetEndOfCurrentQuarter()
+            {
+                return GetEndOfQuarter(DateTime.Now.Year, GetQuarter((Month)DateTime.Now.Month));
+            }
+           
+            public static DateTime GetStartOfLastWeek()
+            {
+                int DaysToSubtract = (int)DateTime.Now.DayOfWeek + 7;
+                DateTime dt = DateTime.Now.Subtract(System.TimeSpan.FromDays(DaysToSubtract));
                 return new DateTime(dt.Year, dt.Month, dt.Day, 0, 0, 0, 0);
             }
 
+            public static DateTime GetEndOfLastWeek()
+            {
+                DateTime dt = GetStartOfLastWeek().AddDays(6);
+                return new DateTime(dt.Year, dt.Month, dt.Day, 23, 59, 59, 999);
+            }
+
+            public static DateTime GetStartOfCurrentWeek()
+            {
+                int DaysToSubtract = (int)DateTime.Now.DayOfWeek;
+                DateTime dt = DateTime.Now.Subtract(System.TimeSpan.FromDays(DaysToSubtract));
+                return new DateTime(dt.Year, dt.Month, dt.Day, 0, 0, 0, 0);
+            }
+
+            public static DateTime GetEndOfCurrentWeek()
+            {
+                DateTime dt = GetStartOfCurrentWeek().AddDays(6);
+                return new DateTime(dt.Year, dt.Month, dt.Day, 23, 59, 59, 999);
+            }
+    
+
+            public static DateTime GetStartOfMonth(Month Month, int Year)
+            {
+                return new DateTime(Year, (int)Month, 1, 0, 0, 0, 0);
+            }
+
+            public static DateTime GetEndOfMonth(Month Month, int Year)
+            {
+                return new DateTime(Year, (int)Month, DateTime.DaysInMonth(Year, (int)Month), 23, 59, 59, 999);
+            }
+
+            public static DateTime GetStartOfLastMonth()
+            {
+                if (DateTime.Now.Month == 1)
+                    return GetStartOfMonth((Month)12, DateTime.Now.Year - 1);
+                else
+                    return GetStartOfMonth((Month)DateTime.Now.Month - 1, DateTime.Now.Year);
+            }
+
+            public static DateTime GetEndOfLastMonth()
+            {
+                if (DateTime.Now.Month == 1)
+                    return GetEndOfMonth((Month)12, DateTime.Now.Year - 1);
+                else
+                    return GetEndOfMonth((Month)DateTime.Now.Month - 1, DateTime.Now.Year);
+            }
+
+            public static DateTime GetStartOfCurrentMonth()
+            {
+                return GetStartOfMonth((Month)DateTime.Now.Month, DateTime.Now.Year);
+            }
+
+            public static DateTime GetEndOfCurrentMonth()
+            {
+                return GetEndOfMonth((Month)DateTime.Now.Month, DateTime.Now.Year);
+            }
+    
+            public static DateTime GetStartOfYear(int Year)
+            {
+                return new DateTime(Year, 1, 1, 0, 0, 0, 0);
+            }
+
+            public static DateTime GetEndOfYear(int Year)
+            {
+                return new DateTime(Year, 12, DateTime.DaysInMonth(Year, 12), 23, 59, 59, 999);
+            }
+
+            public static DateTime GetStartOfLastYear()
+            {
+                return GetStartOfYear(DateTime.Now.Year - 1);
+            }
+
+            public static DateTime GetEndOfLastYear()
+            {
+                return GetEndOfYear(DateTime.Now.Year - 1);
+            }
+
+            public static DateTime GetStartOfCurrentYear()
+            {
+                return GetStartOfYear(DateTime.Now.Year);
+            }
+
+            public static DateTime GetEndOfCurrentYear()
+            {
+                return GetEndOfYear(DateTime.Now.Year);
+            }
+    
+            public static DateTime GetStartOfDay(DateTime date)
+            {
+                return new DateTime(date.Year, date.Month, date.Day, 0, 0, 0, 0);
+            }
+
+            public static DateTime GetEndOfDay(DateTime date)
+            {
+                return new DateTime(date.Year, date.Month, date.Day, 23, 59, 59, 999);
+            }
+       
         #endregion
 
     }
@@ -164,19 +345,19 @@ public static class StringExtensions {
     /// <summary>
     /// Checks if the string contains a numeric value.
     /// </summary>
-    /// <param name="theValue">The string to be tested.</param>
+    /// <param name="str">The string to be tested.</param>
     /// <returns>True if the string is numeric.</returns>
-    public static bool IsNumeric(this string theValue)
+    public static bool IsNumeric(this string str)
     {
-        long retNum;
-        return long.TryParse(theValue, System.Globalization.NumberStyles.Integer, System.Globalization.NumberFormatInfo.InvariantInfo, out retNum);
+        long number;
+        return long.TryParse(str, System.Globalization.NumberStyles.Integer, System.Globalization.NumberFormatInfo.InvariantInfo, out number);
     }
 
     /// <summary>
     /// Counts the words in a string.
     /// </summary>
-    /// <param name="str"></param>
-    /// <returns></returns>
+    /// <param name="str">The string to be tested.</param>
+    /// <returns>The number of words.</returns>
     public static int WordCount(this String str)
     {
         return str.Split(new char[] { ' ', '.', '?' }, StringSplitOptions.RemoveEmptyEntries).Length;
@@ -190,7 +371,7 @@ public static class StringExtensions {
 
         
 
-}
+    }
 #region AgenaTrader Automaticaly Generated Code. Do not change it manualy
 
 namespace AgenaTrader.UserCode
