cb9981b7aa845cd973e79e0868a2420b3a77e264
Simon Pucher <simon.pucher@creativo.at>
2016-04-19 07:44:14 +0200

    DateTimeHelper added
    Many functions added from codeproject Article.

--- Utility/GlobalUtilities_Utility.cs
+++ Utility/GlobalUtilities_Utility.cs
@@ -12,7 +12,7 @@ using AgenaTrader.Plugins;
 using AgenaTrader.Helper;
 
 /// <summary>
-/// Version: 1.0
+/// Version: 1.1
 /// -------------------------------------------------------------------------
 /// Simon Pucher 2016
 /// Christian Kovar 2016
@@ -103,22 +103,205 @@ namespace AgenaTrader.UserCode
             }
 
 
- 
+
         #endregion
 
-        #region DateTime
-        /// <summary>
-        /// Returns the Start of the current week.
-        /// Code from: http://www.codeproject.com/Articles/9706/C-DateTime-Library
-        /// </summary>
-        /// <returns></returns>
-          public static DateTime GetStartOfCurrentWeek()
+        #region DateTimeHelpers taken from  http://www.codeproject.com/Articles/9706/C-DateTime-Library
+
+        public enum Quarter
+        {
+            First = 1,
+            Second = 2,
+            Third = 3,
+            Fourth = 4
+        }
+
+        public enum Month
+        {
+            January = 1,
+            February = 2,
+            March = 3,
+            April = 4,
+            May = 5,
+            June = 6,
+            July = 7,
+            August = 8,
+            September = 9,
+            October = 10,
+            November = 11,
+            December = 12
+        }
+
+            public static DateTime GetStartOfQuarter(int Year, Quarter Qtr)
             {
-                int DaysToSubtract = (int)DateTime.Now.DayOfWeek;
-                DateTime dt = DateTime.Now.Subtract(System.TimeSpan.FromDays( DaysToSubtract ) );
+                if (Qtr == Quarter.First)    // 1st Quarter = January 1 to March 31
+                    return new DateTime(Year, 1, 1, 0, 0, 0, 0);
+                else if (Qtr == Quarter.Second) // 2nd Quarter = April 1 to June 30
+                    return new DateTime(Year, 4, 1, 0, 0, 0, 0);
+                else if (Qtr == Quarter.Third) // 3rd Quarter = July 1 to September 30
+                    return new DateTime(Year, 7, 1, 0, 0, 0, 0);
+                else // 4th Quarter = October 1 to December 31
+                    return new DateTime(Year, 10, 1, 0, 0, 0, 0);
+            }
+
+            public static DateTime GetEndOfQuarter(int Year, Quarter Qtr)
+            {
+                if (Qtr == Quarter.First)    // 1st Quarter = January 1 to March 31
+                    return new DateTime(Year, 3, DateTime.DaysInMonth(Year, 3), 23, 59, 59, 999);
+                else if (Qtr == Quarter.Second) // 2nd Quarter = April 1 to June 30
+                    return new DateTime(Year, 6, DateTime.DaysInMonth(Year, 6), 23, 59, 59, 999);
+                else if (Qtr == Quarter.Third) // 3rd Quarter = July 1 to September 30
+                    return new DateTime(Year, 9, DateTime.DaysInMonth(Year, 9), 23, 59, 59, 999);
+                else // 4th Quarter = October 1 to December 31
+                    return new DateTime(Year, 12, DateTime.DaysInMonth(Year, 12), 23, 59, 59, 999);
+            }
+
+            public static Quarter GetQuarter(Month Month)
+            {
+                if (Month <= Month.March)
+                    // 1st Quarter = January 1 to March 31
+                    return Quarter.First;
+                else if ((Month >= Month.April) && (Month <= Month.June))
+                    // 2nd Quarter = April 1 to June 30
+                    return Quarter.Second;
+                else if ((Month >= Month.July) && (Month <= Month.September))
+                    // 3rd Quarter = July 1 to September 30
+                    return Quarter.Third;
+                else // 4th Quarter = October 1 to December 31
+                    return Quarter.Fourth;
+            }
+
+            public static DateTime GetEndOfLastQuarter()
+            {
+                if ((Month)DateTime.Now.Month <= Month.March)
+                    //go to last quarter of previous year
+                    return GetEndOfQuarter(DateTime.Now.Year - 1, Quarter.Fourth);
+                else //return last quarter of current year
+                    return GetEndOfQuarter(DateTime.Now.Year, GetQuarter((Month)DateTime.Now.Month));
+            }
+
+            public static DateTime GetStartOfLastQuarter()
+            {
+                if ((Month)DateTime.Now.Month <= Month.March)
+                    //go to last quarter of previous year
+                    return GetStartOfQuarter(DateTime.Now.Year - 1, Quarter.Fourth);
+                else //return last quarter of current year
+                    return GetStartOfQuarter(DateTime.Now.Year, GetQuarter((Month)DateTime.Now.Month));
+            }
+
+            public static DateTime GetStartOfCurrentQuarter()
+            {
+                return GetStartOfQuarter(DateTime.Now.Year, GetQuarter((Month)DateTime.Now.Month));
+            }
+
+            public static DateTime GetEndOfCurrentQuarter()
+            {
+                return GetEndOfQuarter(DateTime.Now.Year, GetQuarter((Month)DateTime.Now.Month));
+            }
+           
+            public static DateTime GetStartOfLastWeek()
+            {
+                int DaysToSubtract = (int)DateTime.Now.DayOfWeek + 7;
+                DateTime dt = DateTime.Now.Subtract(System.TimeSpan.FromDays(DaysToSubtract));
                 return new DateTime(dt.Year, dt.Month, dt.Day, 0, 0, 0, 0);
             }
 
+            public static DateTime GetEndOfLastWeek()
+            {
+                DateTime dt = GetStartOfLastWeek().AddDays(6);
+                return new DateTime(dt.Year, dt.Month, dt.Day, 23, 59, 59, 999);
+            }
+
+            public static DateTime GetStartOfCurrentWeek()
+            {
+                int DaysToSubtract = (int)DateTime.Now.DayOfWeek;
+                DateTime dt = DateTime.Now.Subtract(System.TimeSpan.FromDays(DaysToSubtract));
+                return new DateTime(dt.Year, dt.Month, dt.Day, 0, 0, 0, 0);
+            }
+
+            public static DateTime GetEndOfCurrentWeek()
+            {
+                DateTime dt = GetStartOfCurrentWeek().AddDays(6);
+                return new DateTime(dt.Year, dt.Month, dt.Day, 23, 59, 59, 999);
+            }
+    
+
+            public static DateTime GetStartOfMonth(Month Month, int Year)
+            {
+                return new DateTime(Year, (int)Month, 1, 0, 0, 0, 0);
+            }
+
+            public static DateTime GetEndOfMonth(Month Month, int Year)
+            {
+                return new DateTime(Year, (int)Month, DateTime.DaysInMonth(Year, (int)Month), 23, 59, 59, 999);
+            }
+
+            public static DateTime GetStartOfLastMonth()
+            {
+                if (DateTime.Now.Month == 1)
+                    return GetStartOfMonth(12, DateTime.Now.Year - 1);
+                else
+                    return GetStartOfMonth(DateTime.Now.Month - 1, DateTime.Now.Year);
+            }
+
+            public static DateTime GetEndOfLastMonth()
+            {
+                if (DateTime.Now.Month == 1)
+                    return GetEndOfMonth(12, DateTime.Now.Year - 1);
+                else
+                    return GetEndOfMonth(DateTime.Now.Month - 1, DateTime.Now.Year);
+            }
+
+            public static DateTime GetStartOfCurrentMonth()
+            {
+                return GetStartOfMonth(DateTime.Now.Month, DateTime.Now.Year);
+            }
+
+            public static DateTime GetEndOfCurrentMonth()
+            {
+                return GetEndOfMonth(DateTime.Now.Month, DateTime.Now.Year);
+            }
+    
+            public static DateTime GetStartOfYear(int Year)
+            {
+                return new DateTime(Year, 1, 1, 0, 0, 0, 0);
+            }
+
+            public static DateTime GetEndOfYear(int Year)
+            {
+                return new DateTime(Year, 12, DateTime.DaysInMonth(Year, 12), 23, 59, 59, 999);
+            }
+
+            public static DateTime GetStartOfLastYear()
+            {
+                return GetStartOfYear(DateTime.Now.Year - 1);
+            }
+
+            public static DateTime GetEndOfLastYear()
+            {
+                return GetEndOfYear(DateTime.Now.Year - 1);
+            }
+
+            public static DateTime GetStartOfCurrentYear()
+            {
+                return GetStartOfYear(DateTime.Now.Year);
+            }
+
+            public static DateTime GetEndOfCurrentYear()
+            {
+                return GetEndOfYear(DateTime.Now.Year);
+            }
+    
+            public static DateTime GetStartOfDay(DateTime date)
+            {
+                return new DateTime(date.Year, date.Month, date.Day, 0, 0, 0, 0);
+            }
+
+            public static DateTime GetEndOfDay(DateTime date)
+            {
+                return new DateTime(date.Year, date.Month, date.Day, 23, 59, 59, 999);
+            }
+       
         #endregion
 
     }
@@ -132,19 +315,19 @@ public static class StringExtensions {
     /// <summary>
     /// Checks if the string contains a numeric value.
     /// </summary>
-    /// <param name="theValue">The string to be tested.</param>
+    /// <param name="str">The string to be tested.</param>
     /// <returns>True if the string is numeric.</returns>
-    public static bool IsNumeric(this string theValue)
+    public static bool IsNumeric(this string str)
     {
-        long retNum;
-        return long.TryParse(theValue, System.Globalization.NumberStyles.Integer, System.Globalization.NumberFormatInfo.InvariantInfo, out retNum);
+        long number;
+        return long.TryParse(str, System.Globalization.NumberStyles.Integer, System.Globalization.NumberFormatInfo.InvariantInfo, out number);
     }
 
     /// <summary>
     /// Counts the words in a string.
     /// </summary>
-    /// <param name="str"></param>
-    /// <returns></returns>
+    /// <param name="str">The string to be tested.</param>
+    /// <returns>The number of words.</returns>
     public static int WordCount(this String str)
     {
         return str.Split(new char[] { ' ', '.', '?' }, StringSplitOptions.RemoveEmptyEntries).Length;
@@ -158,7 +341,7 @@ public static class StringExtensions {
 
         
 
-}
+    }
 #region AgenaTrader Automaticaly Generated Code. Do not change it manualy
 
 namespace AgenaTrader.UserCode
