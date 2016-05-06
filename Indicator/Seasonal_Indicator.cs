482423219b221bf763f23ed87833916ef445b6da
Simon Pucher <simon.pucher@creativo.at>
2016-05-06 20:33:15 +0200

    First version of the Seasonal indicator

---
+++ Indicator/Seasonal_Indicator.cs
@@ -0,0 +1,228 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Xml;
+using System.Xml.Serialization;
+using AgenaTrader.API;
+using AgenaTrader.Custom;
+using AgenaTrader.Plugins;
+using AgenaTrader.Helper;
+
+
+/// <summary>
+/// Version: in progress
+/// -------------------------------------------------------------------------
+/// Simon Pucher 2016
+/// Christian Kovar 2016
+/// -------------------------------------------------------------------------
+/// The initial version of this strategy was inspired by this link: http://emini-watch.com/stock-market-seasonal-trades/5701/
+/// -------------------------------------------------------------------------
+/// ****** Important ******
+/// To compile this indicator without any error you also need access to the utility indicator to use these global source code elements.
+/// You will find this indicator on GitHub: https://github.com/simonpucher/AgenaTrader/blob/master/Utility/GlobalUtilities_Utility.cs
+/// -------------------------------------------------------------------------
+/// Namespace holds all indicators and is required. Do not change it.
+/// </summary>
+namespace AgenaTrader.UserCode
+{
+    [Description("Show seasonal trends")]
+	public class Seasonal_Indicator : UserIndicator
+	{
+		protected override void Initialize()
+		{
+			Add(new Plot(Color.FromKnownColor(KnownColor.Orange), "MyPlot1"));
+			Overlay = true;
+		}
+
+        protected override void InitRequirements()
+        {
+            //Print("InitRequirements");
+
+        }
+
+        private IEnumerable<IBar> list_sellinmayandgoaway = null;
+        private bool drawing_sellinmayandgoway = false;
+        private IBar last_start_sellinmayandgoway = null;
+
+        protected override void OnStartUp()
+        {
+            //Print("OnStartUp");
+            list_sellinmayandgoaway = Bars.Where(x => x.Time.Month <= 4 || x.Time.Month >= 10);
+        }
+
+		protected override void OnBarUpdate()
+		{
+			MyPlot1.Set(Input[0]);
+
+            if (list_sellinmayandgoaway.Select(x=>x.Time).Contains(Bars[0].Time))
+            {
+                if (last_start_sellinmayandgoway == null)
+                {
+                    this.last_start_sellinmayandgoway = Bars[0];  
+                }
+     
+
+
+            }
+            else
+            {
+                if (last_start_sellinmayandgoway != null)
+                {
+                    IEnumerable<IBar> area = list_sellinmayandgoaway.Where(x => x.Time >= last_start_sellinmayandgoway.Time).Where(x=>x.Time <= Bars[0].Time);
+                    double low = area.Min(x=>x.Low);
+                    double high = area.Max(x=>x.High);
+
+                    DrawRectangle(Bars[0].Time.ToString(), true, this.last_start_sellinmayandgoway.Time, high, Bars[0].Time, low, Color.Green, Color.Green, 70);
+                    last_start_sellinmayandgoway = null;
+                }
+
+            }
+		}
+
+
+
+        public override string ToString()
+        {
+            return "Seasonal";
+        }
+
+        public override string DisplayName
+        {
+            get
+            {
+                return "Seasonal";
+            }
+        }
+
+		#region Properties
+
+		[Browsable(false)]
+		[XmlIgnore()]
+		public DataSeries MyPlot1
+		{
+			get { return Values[0]; }
+		}
+
+		#endregion
+	}
+}
+#region AgenaTrader Automaticaly Generated Code. Do not change it manualy
+
+namespace AgenaTrader.UserCode
+{
+	#region Indicator
+
+	public partial class UserIndicator : Indicator
+	{
+		/// <summary>
+		/// Show seasonal trends
+		/// </summary>
+		public Seasonal_Indicator Seasonal_Indicator()
+        {
+			return Seasonal_Indicator(Input);
+		}
+
+		/// <summary>
+		/// Show seasonal trends
+		/// </summary>
+		public Seasonal_Indicator Seasonal_Indicator(IDataSeries input)
+		{
+			var indicator = CachedCalculationUnits.GetCachedIndicator<Seasonal_Indicator>(input);
+
+			if (indicator != null)
+				return indicator;
+
+			indicator = new Seasonal_Indicator
+						{
+							BarsRequired = BarsRequired,
+							CalculateOnBarClose = CalculateOnBarClose,
+							Input = input
+						};
+			indicator.SetUp();
+
+			CachedCalculationUnits.AddIndicator2Cache(indicator);
+
+			return indicator;
+		}
+	}
+
+	#endregion
+
+	#region Strategy
+
+	public partial class UserStrategy
+	{
+		/// <summary>
+		/// Show seasonal trends
+		/// </summary>
+		public Seasonal_Indicator Seasonal_Indicator()
+		{
+			return LeadIndicator.Seasonal_Indicator(Input);
+		}
+
+		/// <summary>
+		/// Show seasonal trends
+		/// </summary>
+		public Seasonal_Indicator Seasonal_Indicator(IDataSeries input)
+		{
+			if (InInitialize && input == null)
+				throw new ArgumentException("You only can access an indicator with the default input/bar series from within the 'Initialize()' method");
+
+			return LeadIndicator.Seasonal_Indicator(input);
+		}
+	}
+
+	#endregion
+
+	#region Column
+
+	public partial class UserColumn
+	{
+		/// <summary>
+		/// Show seasonal trends
+		/// </summary>
+		public Seasonal_Indicator Seasonal_Indicator()
+		{
+			return LeadIndicator.Seasonal_Indicator(Input);
+		}
+
+		/// <summary>
+		/// Show seasonal trends
+		/// </summary>
+		public Seasonal_Indicator Seasonal_Indicator(IDataSeries input)
+		{
+			return LeadIndicator.Seasonal_Indicator(input);
+		}
+	}
+
+	#endregion
+
+	#region Scripted Condition
+
+	public partial class UserScriptedCondition
+	{
+		/// <summary>
+		/// Show seasonal trends
+		/// </summary>
+		public Seasonal_Indicator Seasonal_Indicator()
+		{
+			return LeadIndicator.Seasonal_Indicator(Input);
+		}
+
+		/// <summary>
+		/// Show seasonal trends
+		/// </summary>
+		public Seasonal_Indicator Seasonal_Indicator(IDataSeries input)
+		{
+			return LeadIndicator.Seasonal_Indicator(input);
+		}
+	}
+
+	#endregion
+
+}
+
+#endregion
