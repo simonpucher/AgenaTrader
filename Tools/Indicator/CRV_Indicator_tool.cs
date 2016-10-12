7f06195081181e6d599a989b681695a515267fb5
simonpucher <simon.pucher@creativo.at>
2016-10-12 16:19:13 +0200

    calculation on position size and price

--- Tools/Indicator/CRV_Indicator_tool.cs
+++ Tools/Indicator/CRV_Indicator_tool.cs
@@ -12,13 +12,12 @@ using AgenaTrader.Plugins;
 using AgenaTrader.Helper;
 
 /// <summary>
-/// Version: 1.3.5
+/// Version: 1.3.6
 /// -------------------------------------------------------------------------
 /// Simon Pucher 2016
 /// -------------------------------------------------------------------------
 /// todo
 /// + crv calculation not working on proposals
-/// + if there are positions left in the portfolio write + symbol
 /// -------------------------------------------------------------------------
 /// Shows the CRV of your current trade in the right upper corner of the chart.
 /// -------------------------------------------------------------------------
@@ -26,6 +25,27 @@ using AgenaTrader.Helper;
 /// </summary>
 namespace AgenaTrader.UserCode
 {
+
+
+    public class crv_resultobject {
+
+        public string text = "flat";
+        public double crv = 0.0;
+       public double crv_price = 0.0;
+        public double up = 0.0;
+        public double down = 0.0;
+        public double up_price = 0.0;
+        public double down_price = 0.0;
+
+        public double target_quant = 0.0;
+        public double stop_quant = 0.0;
+
+        public crv_resultobject() {
+
+        }
+    }
+
+
 	[Description("Shows the CRV of your current trade in the right upper corner of the chart.")]
     [Category("Tools")]
     public class CRV_Indicator_Tool : UserIndicator
@@ -77,9 +97,9 @@ namespace AgenaTrader.UserCode
 
             if (force || _lastupdate.AddSeconds(this._seconds) < DateTime.Now)
             {
-                string text = "flat";
 
-               
+
+
                 //openedtrade = this.Root.Core.TradingManager.GetOpenedTrade(this.Instrument);
                 //IEnumerable<ITradingOrder> SubmittedOrders = this.Root.Core.TradingManager.SubmittedOrders;
                 //IEnumerable<ITradingOrder> RegisteredOrders = this.Root.Core.TradingManager.RegisteredOrders;
@@ -88,108 +108,24 @@ namespace AgenaTrader.UserCode
                 //IEnumerable<ITradingOrder> ActiveRegisteredOrders = this.Root.Core.TradingManager.ActiveRegisteredOrders;
                 //IEnumerable<ITradingOrder> ActiveSubmittedOrders = this.Root.Core.TradingManager.ActiveSubmittedOrders;
 
-
-                double crv = 0.0;
-                double crv_price = 0.0;
-                double up = 0.0;
-                double down = 0.0;
-                double up_price = 0.0;
-                double down_price = 0.0;
+                crv_resultobject resultdata = new crv_resultobject();
 
 
-                //IEnumerable<ITradingOrder> _regorders = this.Root.Core.TradingManager.ActiveRegisteredOrders.Where(x => x.Instrument.Symbol == this.Instrument.Symbol);
-                //if (_regorders != null && _regorders.Count() > 0)
-                //{
-                //    PositionType direction = PositionType.Flat;
 
-                //    foreach (ITradingOrder item in _regorders)
-                //    {
-
-                //    }
-
-                //}
+               
 
 
 
                 IEnumerable<ITradingOrder> _openorders = this.Root.Core.TradingManager.OpenedOrders.Where(x => x.Instrument.Symbol == this.Instrument.Symbol);
 
-                //IEnumerable<ITradingOrder> combine = _openorders.Union(_regorders);
-
-                if (TradeInfo != null && _openorders != null && _openorders.Count() > 0)
+                
+                if (this.TradeInfo != null)
                 {
-                    
-                    foreach (ITradingOrder item in _openorders)
-                    {
-                        if (TradeInfo.MarketPosition == PositionType.Long && item.IsLong)
-                        {
-
-                        }
-                        else if (TradeInfo.MarketPosition == PositionType.Short && item.IsShort)
-                        {
-
-                        }
-                        else
-                        {
-                            double price = item.StopPrice;
-                            if (price == 0.0)
-                            {
-                                price = item.Price;
-                            }
-                            //stop or target
-                            if (price < TradeInfo.AvgPrice && TradeInfo.MarketPosition == PositionType.Long
-                                || price > TradeInfo.AvgPrice && TradeInfo.MarketPosition == PositionType.Short)
-                            {
-                                down_price = down_price + (TradeInfo.AvgPrice - item.StopPrice);
-                                down = down + ((TradeInfo.AvgPrice * item.Quantity) - (item.StopPrice * item.Quantity));
-                            }
-                            else
-                            {
-                                up_price = up_price + (item.Price - TradeInfo.AvgPrice);
-                                up = up + ((item.Price * item.Quantity) - (TradeInfo.AvgPrice * item.Quantity));
-                            }
-                        }
-                    }
-                    
+                    resultdata = this.calculate(_openorders, this.TradeInfo.Quantity, TradeInfo.AvgPrice, TradeInfo.MarketPosition);
                 }
 
 
-
-              
-
-                crv = up / down;
-                crv_price = up_price / down_price;
-
-                if (down == 0.0 && up != 0.0)
-                {
-                    //text = "in risk";
-                    text = "-";
-                }
-                else if (down != 0.0 && up == 0.0)
-                {
-                    //text = "in love";
-                    text = "-";
-                }
-                else if (down == 0.0 && up == 0.0)
-                {
-                    if (TradeInfo != null)
-                    {
-                        switch (TradeInfo.MarketPosition)
-                        {
-                            case PositionType.Long:
-                                text = "long";
-                                break;
-                            case PositionType.Short:
-                                text = "short";
-                                break;
-                        }
-                    }
-                }
-                else
-                {
-                    text = Math.Round(crv, this.RoundDecimal).ToString() + " / " + Math.Round(crv_price, this.RoundDecimal).ToString();
-                }
-               
-                DrawTextFixed("CRV_string", text, this.TextPositionCRV, Color.Black, new Font("Arial", this.FontSizeCRV, FontStyle.Regular), Color.Transparent, Color.Transparent);
+                DrawTextFixed("CRV_string", resultdata.text, this.TextPositionCRV, Color.Black, new Font("Arial", this.FontSizeCRV, FontStyle.Regular), Color.Transparent, Color.Transparent);
                 _lastupdate = DateTime.Now;
             }
            
@@ -202,7 +138,97 @@ namespace AgenaTrader.UserCode
             if (ChartControl != null)
                 ChartControl.ChartPanelMouseMove -= ChartControl_ChartPanelMouseMove;
         }
-        
+
+
+        public crv_resultobject calculate(IEnumerable<ITradingOrder> _openorders, int entry_quantity, double entry_price, PositionType positiontype) {
+            crv_resultobject result = new crv_resultobject();
+
+            if ( _openorders != null && _openorders.Count() > 0)
+            {
+                result.target_quant = entry_quantity;
+                result.stop_quant = entry_quantity;
+
+                foreach (ITradingOrder item in _openorders)
+                {
+                    if ((positiontype == PositionType.Long && item.IsLong) || (positiontype == PositionType.Short && item.IsShort))
+                    {
+                        //
+                    }
+                    else
+                    {
+                        double price = item.StopPrice;
+                        if (price == 0.0)
+                        {
+                            price = item.Price;
+                        }
+                        //stop or target
+                        if (price < entry_price && positiontype == PositionType.Long
+                            || price > entry_price && positiontype == PositionType.Short)
+                        {
+                            result.stop_quant = result.stop_quant - item.Quantity;
+                            result.down_price = result.down_price + (entry_price - item.StopPrice);
+                            result.down = result.down + ((entry_price * item.Quantity) - (item.StopPrice * item.Quantity));
+                        }
+                        else
+                        {
+                            result.target_quant = result.target_quant - item.Quantity;
+                            result.up_price = result.up_price + (item.Price - entry_price);
+                            result.up = result.up + ((item.Price * item.Quantity) - (entry_price * item.Quantity));
+                        }
+                    }
+                }
+
+            }
+            
+
+            result.crv = result.up / result.down;
+            result.crv_price = result.up_price / result.down_price;
+
+            if (result.down == 0.0 && result.up != 0.0)
+            {
+                //text = "in risk";
+                result.text = "-";
+            }
+            else if (result.down != 0.0 && result.up == 0.0)
+            {
+                //text = "in love";
+                result.text = "-";
+            }
+            else if (result.down == 0.0 && result.up == 0.0)
+            {
+                if (TradeInfo != null)
+                {
+                    switch (positiontype)
+                    {
+                        case PositionType.Long:
+                            result.text = "long";
+                            break;
+                        case PositionType.Short:
+                            result.text = "short";
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                string textquant = "";
+                if (result.target_quant > 0 && result.stop_quant > 0)
+                {
+                    textquant = "*";
+                }
+                else if (result.target_quant > 0)
+                {
+                    textquant = "+";
+                }
+                else if (result.stop_quant > 0)
+                {
+                    textquant = "-";
+                }
+                result.text = Math.Round(result.crv, this.RoundDecimal).ToString() + textquant + " / " + Math.Round(result.crv_price, this.RoundDecimal).ToString();
+            }
+
+            return result;
+        }
 
 
         public override string ToString()
